a7753f277d673c2b132b35d018330e52dfae7b3b
keegang6705 <darunphobwi@gmail.com>
2025-03-29 20:22:14 +0700

    eh

--- popup/popup.js
+++ popup/popup.js
@@ -77,7 +77,7 @@ for (var i = 0; i < settingCheckboxes.length; i++) {
 
 feature_btn.addEventListener("click", function() {
   chrome.tabs.create({
-    url: 'https://raw.githubusercontent.com/keegang6705/BemisEditor/master/source/features.mp4' 
+    url: 'https://raw.githubusercontent.com/keegang6705/BemisEditor/master/source/feature.mp4' 
    });
 });
 
